591206f4f23276409d699dba38dd219fd318be82
Crosstyan <cf595833532@gmail.com>
2023-10-11 01:58:20 +0800

    custom unmarshal

--- description/request_record.go
+++ description/request_record.go
@@ -2,30 +2,66 @@ package description
 
 import (
 	"encoding/json"
+	"net/http"
 	"net/url"
 )
 
-type Url struct {
-	Url url.URL
-}
-
 type RequestRecord struct {
-	Cookies []TempCookie      `json:"cookies"`
+	Cookies []http.Cookie     `json:"cookies"`
 	Headers map[string]string `json:"headers"`
 }
 
-type Description struct {
-	Images   []Url                    `json:"images"`
-	Requests map[string]RequestRecord `json:"requests"`
-}
-
-func (u *Url) UnmarshalJSON(data []byte) error {
-	var s string
-	err := json.Unmarshal(data, &s)
+func (r *RequestRecord) UnmarshalJSON(data []byte) error {
+	var raw map[string]json.RawMessage
+	err := json.Unmarshal(data, &raw)
 	if err != nil {
 		return err
 	}
-	rawUrl, err := url.Parse(s)
-	u.Url = *rawUrl
-	return err
+
+	temp := make([]TempCookie, 0)
+	err = json.Unmarshal(raw["cookies"], &temp)
+	if err != nil {
+		return err
+	}
+	for _, v := range temp {
+		r.Cookies = append(r.Cookies, v.ToNetCookie())
+	}
+
+	err = json.Unmarshal(raw["headers"], &r.Headers)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type Description struct {
+	Images   []url.URL                `json:"images"`
+	Requests map[string]RequestRecord `json:"requests"`
+}
+
+func (d *Description) UnmarshalJSON(data []byte) error {
+	var raw map[string]json.RawMessage
+	err := json.Unmarshal(data, &raw)
+	if err != nil {
+		return err
+	}
+
+	temp := make([]string, 0)
+	err = json.Unmarshal(raw["images"], &temp)
+	if err != nil {
+		return err
+	}
+	for _, v := range temp {
+		u, err := url.Parse(v)
+		if err != nil {
+			return err
+		}
+		d.Images = append(d.Images, *u)
+	}
+
+	err = json.Unmarshal(raw["requests"], &d.Requests)
+	if err != nil {
+		return err
+	}
+	return nil
 }
